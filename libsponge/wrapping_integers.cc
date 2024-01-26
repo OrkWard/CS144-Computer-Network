85b4756873eb6bcc4415abb73b333feba733fded
OrkWard <orkward_o@outlook.com>
2024-01-26 19:02:06 +0800

    feat(lab2): finish wrap_integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,22 +1,15 @@
 #include "wrapping_integers.hh"
 
-// Dummy implementation of a 32-bit wrapping integer
-
-// For Lab 2, please replace with a real implementation that passes the
-// automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32{static_cast<uint32_t>(n) + isn.raw_value()}; }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +22,20 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // Calculate wrapped absolute n
+    uint32_t wrap_abs_n = n.raw_value() - isn.raw_value();
+    // This is one of the candidates. The distance between this and checkpoint
+    // must be less than 2^32.
+    uint64_t unwarp_n = (checkpoint & 0xFFFFFFFF00000000u) + static_cast<uint64_t>(wrap_abs_n);
+
+    cout << setbase(16) << unwarp_n << endl;
+
+    if (unwarp_n > checkpoint && unwarp_n - checkpoint > 0x80000000u && unwarp_n > (1ul << 32))
+        // too large
+        return unwarp_n - (1ul << 32);
+    else if (unwarp_n < checkpoint && checkpoint - unwarp_n > 0x80000000u && unwarp_n + (1ul << 32) < (0ul - 1))
+        // too small
+        return unwarp_n + (1ul << 32);
+    else
+        return unwarp_n;
 }
